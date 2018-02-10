a8177a830aa240687d6b0e69c7e4f03e4b4ebb75
Ulises <ulises@Ulises.local>
2018-02-10 11:19:52 -0500

    Fix api url

--- src/app/setup/config.ts
+++ src/app/setup/config.ts
@@ -10,7 +10,7 @@ export const storage: StorageConfig = {}
  * API.
  */
 export const api: APIConfig = {
-  dev: '',
-  staging: '',
-  production: '',
+  dev: ' http://54.218.90.55/',
+  staging: ' http://54.218.90.55/',
+  production: ' http://54.218.90.55/',
 }
