1967397d3ce03f7a1d1416b4fb46c634e2995904
Ulises <ulises@Ulises.local>
2018-02-16 16:13:37 -0500

    Fix create students screen

--- src/views/modal/modals/new-student-modal/new-student-modal.tsx
+++ src/views/modal/modals/new-student-modal/new-student-modal.tsx
@@ -6,18 +6,37 @@ import { Button } from '../../../shared/button'
 import { Text } from '../../../shared/text'
 import { TextField } from '../../../shared/text-field'
 import { ModalStore } from '../../../../models/modal-store'
+import { StudentStore } from '../../../../models/student-store'
 import * as screenStyles from './new-student-modal.styles'
 import { color } from '../../../theme'
 
 export interface NewStudentModalProps {
   modalStore?: ModalStore
+  studentStore?: StudentStore
+}
+
+export interface NewStudentModalState {
+  firstName: string
+  lastName: string
 }
 
 @inject('modalStore')
+@inject('studentStore')
 @observer
-export class NewStudentModal extends React.Component<NewStudentModalProps, {}> {
+export class NewStudentModal extends React.Component<NewStudentModalProps, NewStudentModalState> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      firstName: '',
+      lastName: '',
+    }
+  }
+
+  createStudent = () => {}
+
   render() {
     const { close } = this.props.modalStore
+    const { firstName, lastName } = this.state
     const isCreating = false
 
     return (
@@ -30,30 +49,30 @@ export class NewStudentModal extends React.Component<NewStudentModalProps, {}> {
         </View>
         <View style={screenStyles.container}>
           <TextField
-            value={''}
+            value={firstName}
             labelTx="common.firstName"
             placeholderTx="common.firstNamePlaceholder"
-            onChangeText={e => {}}
             autoCapitalize="none"
             autoCorrect={false}
             editable={!isCreating}
             style={screenStyles.inputTextField}
             inputStyle={screenStyles.inputText}
             returnKeyType="next"
+            onChangeText={e => this.setState({ firstName: e })}
             onSubmitEditing={() => {}}
             blurOnSubmit={false}
           />
           <TextField
-            value={''}
+            value={lastName}
             labelTx="common.lastName"
             placeholderTx="common.lastNamePlaceholder"
-            onChangeText={e => {}}
             autoCapitalize="none"
             autoCorrect={false}
             editable={!isCreating}
             style={screenStyles.inputTextField}
             inputStyle={screenStyles.inputText}
             returnKeyType="next"
+            onChangeText={e => this.setState({ lastName: e })}
             onSubmitEditing={() => {}}
             blurOnSubmit={false}
           />
